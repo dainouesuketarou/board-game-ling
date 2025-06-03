606943ce15b57cf02c638cafc656173ad6462495
Daiki <gotodaiki1110@icloud.com>
2025-06-04 01:45:34 +0900

    eehh

--- src/utils/p2pConnection.ts
+++ src/utils/p2pConnection.ts
@@ -43,20 +43,12 @@ export class P2PConnection {
               { urls: 'stun:stun1.l.google.com:19302' },
               { urls: 'stun:stun2.l.google.com:19302' },
               { urls: 'stun:stun3.l.google.com:19302' },
-              { urls: 'stun:stun4.l.google.com:19302' },
-              { urls: 'stun:stun.ekiga.net' },
-              { urls: 'stun:stun.ideasip.com' },
-              { urls: 'stun:stun.schlund.de' },
-              { urls: 'stun:stun.stunprotocol.org:3478' },
-              { urls: 'stun:stun.voiparound.com' },
-              { urls: 'stun:stun.voipbuster.com' },
-              { urls: 'stun:stun.voipstunt.com' },
-              { urls: 'stun:stun.voxgratia.org' }
+              { urls: 'stun:stun4.l.google.com:19302' }
             ]
           },
           debug: 2,
           secure: true,
-          host: 'peerjs-server.herokuapp.com',
+          host: '0.peerjs.com',
           port: 443,
           path: '/',
           pingInterval: 5000
